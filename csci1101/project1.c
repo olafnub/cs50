e2906bf86ea1cc7964cf282167810c6ccb2e737f
olafnub <googlhoogle@gmail.com>
2023-04-19 12:17:16 -0500

    Finish project 1 for CSCI 1101

--- csci1101/project1.c
+++ csci1101/project1.c
@@ -8,3 +8,15 @@
 
 // April 10. 2023
 
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Andy Li\n\n");
+    // Double space
+    printf("CSCI 1101\n");
+    printf("Introduction to Programming\n");
+    printf("Project 1\n\n");
+    // Double space
+    printf("April 19, 2023\n");
+}
